202990f721c4ba9189766bdc39c772afdc22cb19
Scott A Carr <s.carr1024@gmail.com>
2017-10-17 21:28:40 -0400

    add some board functions

--- dice/dice.rkt
+++ dice/dice.rkt
@@ -3,9 +3,17 @@
 
 ; graphics
 (define SIDE 75)
+(define OFFSET0 (* 2 SIDE))
 (define ROTATION 30)
 (define DICE-IMAGE (bitmap "graphics/dice1.png"))
 (define FOCUS (rotate ROTATION (regular-polygon SIDE 6 "outline" "black")))
+(define COLORS 
+  (list (make-color 255 0 0 100) 
+        (make-color 0 255 0 100) 
+        (make-color 0 0 255 100)))
+(define HEX 6)
+(define (hexagon color)
+  (rotate ROTATION (regular-polygon SIDE HEX "solid" color)))
 
 ; game constants
 (define PLAYER# 2)
@@ -14,31 +22,103 @@
 (define GRID (* BOARD BOARD))
 (define INIT-PLAYER 0)
 (define INIT-SPARE-DICE 10)
+(define X-OFFSET (image-width (hexagon "black")))
+(define Y-OFFSET (* (image-height (hexagon "black")) 3/4))
 
-(define INSTRUCTIONS (text INSTRUCT TEXT-SIZE TEXT-COLOR))
-(define WIDTH (+ (image-width INSTRUCTIONS) 50))
 (define HEIGHT 600)
 (define TEXT-SIZE 25)
 (define TEXT-COLOR "black")
 (define INSTRUCT 
   "← and → to move among territories, <enter> to mark, <d> to unmark, and <p> to pass")
+(define INSTRUCTIONS (text INSTRUCT TEXT-SIZE TEXT-COLOR))
+(define WIDTH (+ (image-width INSTRUCTIONS) 50))
+
 (define AI-TURN "It's the Mighty AI's turn")
 (define YOUR-TURN "It's your turn")
 (define INFO-X-OFFSET 100)
 (define INFO-Y-OFFSET 50)
 
 ; structs
-(struct action (player position))
-(struct dice-world (src board gt))
-(struct territory (index player dice x y))
-(struct game (board player moves))
-(struct move (action gt))
+(struct action (player position) #:transparent)
+(struct dice-world (src board gt) #:transparent)
+(struct territory (index player dice x y) #:transparent)
+(struct game (board player moves) #:transparent)
+(struct move (action gt) #:transparent)
 
 (define (draw-focus marked? p-in-focus p t-image)
   (if (or (and (not marked?) (= p-in-focus p))
           (and marked? (not (= p-in-focus p))))
       (overlay FOCUS t-image)
       (t-image)))
+
+(define (get-dice-image n)
+  DICE-IMAGE)
+
+(define (switch player)
+  (modulo (add1 player) PLAYER#))
+
+(define (add-dice-to t)
+  (territory-set-dice t (add1 (territory-dice t))))
+
+(define (add b x)
+  (if b empty (list x)))
+
+(define (even-row pos top? bottom? right? left?)
+  ;; TODO )
+	 
+(define (neighbors pos)
+  (define top? (< pos BOARD))
+  (define bottom? (= (get-row pos) (sub1 BOARD)))
+  (define even-row? (zero? (modulo (get-row pos) 2)))
+  (define right? (zero? (modulo (add1 pos) BOARD)))
+  (define left? (zero? (modulo pos BOARD)))
+  (if even-row?
+      (even-row pos top? bottom? right? left?)
+      (odd-row pos top? bottom? right? left?)))
+  
+(define (distribute board player spare-dice)
+  (for/fold ([dice spare-dice] [new-board '()]) ([t board])
+    (if (and (= (territory-player t) player)
+	     (< (territory-dice t) DICE#)
+	     (not (zero? dice)))
+	(values (- dice 1) (cons (add-dice-to t) new-board))
+	(values dice (cons t new-board)))))
+
+(define (game-tree board player dice)
+  ;; create tree of attacks from this positiion; add passing move
+  (define (attacks board)
+    (for*/list ([src board]
+		[dst (neighbors (territory-index src))]
+                #:when (attackable? board player src dst))
+      (define from (territory-index src))
+      (define dice (territory-dice src))
+      (define newb (execute board player from dst dice))
+      (move (list from dst) (game newb player more))))
+  ;; create a passing move and the rest of the game tree
+  (define (passes board)
+    (define-values (new-dice newb) (distribute board player dice))
+    (move '() (game-tree newb (switch player) new-dice)))
+  ;; -- START: --
+  (game board player (attacks board)))
+ 
+
+(define (draw-dice dice)
+  (define first-dice (get-dice-image 0))
+  (define height-dice (image-height first-dice))
+  (for/fold ([s first-dice]) ([i (- dice 1)])
+    (define dice-image (get-dice-image (+ i 1)))
+    (define y-offset (* height-dice (+ .5 (* i .25))))
+    (overlay/offset s 0 y-offset dice-image)))
+
+(define (color-chooser n)
+  (list-ref COLORS n))
+
+(define (add-territory t image scene)
+  (place-image image (territory-x t) (territory-y t) scene))
+
+(define (draw-territory t)
+  (define color (color-chooser (territory-player t)))
+  (overlay (hexagon color) (draw-dice (territory-dice t))))
 
 (define (add-board-to-scene w s)
   (define board (dice-world-board w))
@@ -80,3 +160,27 @@
   (set! WIDTH  (+ (max iw bw) 50))
   (set! HEIGHT (+ (* SIDE 2 BOARD) 50))
   (empty-scene WIDTH HEIGHT))
+
+(define (dice)
+  (add1 (random DICE#)))
+
+(define (get-row pos)
+  (quotient pos BOARD))
+
+(define (get-x n)
+  (+ OFFSET0
+     (if (odd? (get-row n)) 0 (/ X-OFFSET 2))
+     (* X-OFFSET (modulo n BOARD))))
+
+(define (get-y n)
+  (+ OFFSET0 (* Y-OFFSET (get-row n))))
+
+(define (territory-build)
+  (for/list ([n (in-range GRID)])
+    (territory n (modulo n PLAYER#) (dice) (get-x n) (get-y n))))
+
+(define (create-world-of-dice-and-doom)
+  (define board (territory-build))
+  (define gamet (game-tree board INIT-PLAYER INIT-SPARE-DICE))
+  (define new-world (dice-world #f board gamet))
+  new-world)
